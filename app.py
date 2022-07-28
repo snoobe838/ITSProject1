dfacb843833dc82b1db0618322056080d85be3ea
SHUBHAM PAREEK <shubhampareek838@gmail.com>
2022-07-28 08:44:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,4 +27,5 @@ def predict():
     return render_template('index.html', prediction_text='Regression Model  has predicted salary for given experinace is : {}'.format(prediction))
 
 
-app.run()
+if __name__ == "__main__":
+  app.run(debug = True)
